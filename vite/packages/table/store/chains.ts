9cbb20e617619510a287e28c4af51944c347294c
teddycode <1055334354@qq.com>
2024-03-06 16:36:25 +0800

    feat: upgrade walletconnect version

--- vite/packages/table/store/chains.ts
+++ vite/packages/table/store/chains.ts
@@ -34,7 +34,7 @@ export const goerli: ChainNetInfo = {
 export const punkos: ChainNetInfo = {
   chainId: 123456789,
   name: 'PunkOS Test Network',
-  currency: 'PTH',
+  currency: 'ETH',
   explorerUrl: 'https://goerli.etherscan.io',
   rpcUrl: 'https://rpctest.byebyegfw.uk',
 };
