245405b35b9cd03a454a624c103bcb6f46d95823
luckysheep <2906997669@qq.com>
2025-01-07 19:56:29 +0800

    存储组前端接入

--- vite/packages/table/route/core/storage.ts
+++ vite/packages/table/route/core/storage.ts
@@ -3,6 +3,7 @@ import OrdersPage from '@page/core/Storage/offchainStorage/Orders.vue';
 import ResourcePage from '@page/core/Storage/offchainStorage/Resource.vue';
 import ThirdLayout from '@page/core/Layouts/ThirdLayout.vue';
 import { CodeOutlined } from '@ant-design/icons-vue';
+import browser from "@js/common/browser";
 
 export default {
   path: 'storage',
@@ -38,6 +39,18 @@ export default {
       meta: {
         title: '链下存储区资源提供者',
         icon: CodeOutlined,
+      },
+    },
+    {
+      path: '/selfStorage',
+      name: 'selfStorage',
+      meta: {
+        title: '私有存储面板',
+        icon: CodeOutlined,
+      },
+      beforeEnter(){
+        browser.openInTable('http://111.119.239.159:36014/group-storage/#/dashboard/upload', { wallet: true });
+        // window.location.href = 'https://punk.buaadcl.tech/group-storage/#/dashboard/upload';
       },
     },
   ],
